70ae7336e796d91d65ff6af143bf97cda60df046
Satwik Bhimavarapu <satwik_bh@hotmail.com>
2017-02-26 20:16:24 +0530

    Corrected the path for dumps

--- DimensionalityReduction/LoadDataSpark.py
+++ DimensionalityReduction/LoadDataSpark.py
@@ -2,6 +2,7 @@ import json
 import logging.config
 import os
 import pickle as pi
+import time
 
 import numpy as np
 from pyspark import SparkContext
@@ -36,12 +37,12 @@ class LoadData:
             logging.basicConfig(level=default_level)
 
     @staticmethod
-    def create_dumps(input_matrix, names_list):
+    def create_dumps(data_path, input_matrix, names_list):
         LoadData.logger.info("Entering the {} class".format(LoadData.create_dumps.__name__))
         LoadData.logger.info("Creating the Dumps")
 
-        pi.dump(input_matrix, open("input_matrix", "w"))
-        pi.dump(names_list, open("names_list", "w"))
+        pi.dump(input_matrix, open(data_path + "/" + "input_matrix", "w"))
+        pi.dump(names_list, open(data_path + "/" + "names_list", "w"))
 
         LoadData.logger.info("Completed creating the Dumps")
         LoadData.logger.info("Entering the {} class".format(LoadData.create_dumps.__name__))
@@ -62,16 +63,18 @@ class LoadData:
         input_matrix = np.array(l)
         names = np.array(names)
 
-        LoadData.create_dumps(input_matrix, names)
+        LoadData.create_dumps(data_path, input_matrix, names)
         LoadData.logger.info("Exiting the {} class".format(LoadData.load_data.__name__))
 
     def main(self):
+        start_time = time.time()
         LoadData.logger.info("Entering the {} class".format(LoadData.main.__name__))
         print("Enter the path to load data from")
         data_path = str(raw_input())
         self.load_data(data_path)
         print("Creating data is completed.")
         LoadData.logger.info("Exiting the {} class".format(LoadData.main.__name__))
+        LoadData.logger.info("Total time taken : {}".format(time.time() - start_time))
 
 
 if __name__ == '__main__':
