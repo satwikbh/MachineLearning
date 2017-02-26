aecd2a2e1c169e596faaccec252a403687871b63
Satwik Bhimavarapu <satwik_bh@hotmail.com>
2017-02-26 19:34:59 +0530

    Developed a spark version for loading the data and creating the input_matrix

---
+++ DimensionalityReduction/LoadDataSpark.py
@@ -0,0 +1,78 @@
+import json
+import logging.config
+import os
+import pickle as pi
+
+import numpy as np
+from pyspark import SparkContext as sc
+
+
+class LoadData:
+    logger = logging.getLogger(__name__)
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def setup_logging(default_path='logging.json', default_level=logging.INFO, env_key='LOG_CFG'):
+        """
+        Setup logging configuration
+        :param default_path:
+        :param default_level:
+        :param env_key:
+        :return:
+        """
+        path = default_path
+        value = os.getenv(env_key, None)
+        if value:
+            path = value
+        if os.path.exists(path):
+            with open(path, 'rt') as f:
+                config = json.load(f)
+            logging.config.dictConfig(config)
+        else:
+            logging.basicConfig(level=default_level)
+
+    @staticmethod
+    def create_dumps(input_matrix, names_list):
+        LoadData.logger.info("Entering the {} class".format(LoadData.create_dumps.__name__))
+        LoadData.logger.info("Creating the Dumps")
+
+        pi.dump(input_matrix, open("input_matrix", "w"))
+        pi.dump(names_list, open("names_list", "w"))
+
+        LoadData.logger.info("Completed creating the Dumps")
+        LoadData.logger.info("Entering the {} class".format(LoadData.create_dumps.__name__))
+
+    @staticmethod
+    def load_data(data_path):
+        LoadData.logger.info("Entering the {} class".format(LoadData.load_data.__name__))
+        rdd = sc.textFile(data_path)
+        length = rdd.map(lambda line: line.split(",")[1][:-2]).filter(lambda line: list(line)).count()
+        l = list(list())
+
+        names = rdd.map(lambda line: line.split(",")[0]).take(length)
+        temp_list = rdd.map(lambda line: line.split(",")[1][:-2]).take(length)
+
+        for each in temp_list:
+            l.append(list(each))
+
+        input_matrix = np.array(l)
+        names = np.array(names)
+
+        LoadData.create_dumps(input_matrix, names)
+        LoadData.logger.info("Exiting the {} class".format(LoadData.load_data.__name__))
+
+    def main(self):
+        LoadData.logger.info("Entering the {} class".format(LoadData.main.__name__))
+        print("Enter the path to load data from")
+        data_path = str(raw_input())
+        self.load_data(data_path)
+        print("Creating data is completed.")
+        LoadData.logger.info("Exiting the {} class".format(LoadData.main.__name__))
+
+
+if __name__ == '__main__':
+    ld = LoadData()
+    ld.setup_logging()
+    ld.main()
