82f8c2351c5bcbd49c659aaeb3fa921237a09c43
Satwik Bhimavarapu <satwik_bh@hotmail.com>
2017-03-19 21:05:12 +0530

    Added the configuration file. From now the cluster results will be fetched from Mongo.

--- FeatureVectorPreparation/FeatureVectorGeneration.py
+++ FeatureVectorPreparation/FeatureVectorGeneration.py
@@ -1,5 +1,4 @@
 import ConfigParser
-import csv
 import logging
 import os
 import pickle as pi
@@ -7,11 +6,12 @@ import pickle as pi
 from pymongo import MongoClient
 
 config = ConfigParser.RawConfigParser()
-config.read('Config.properties')
+config.read('../Config.properties')
 
 from collections import defaultdict
 
 log = logging.getLogger(__name__)
+
 LOG_FILENAME = config.get('Mappings', 'mappings.logfile')
 LOG_LEVEL = config.get('Mappings', 'loglevel')
 
@@ -42,12 +42,12 @@ class FeatureVectorGeneration():
     @staticmethod
     def get_client():
         return MongoClient(
-            "mongod://" + config.get("MongoProperties", "address") + ":" + config.get("MongoProperties", "port"))
+            "mongodb://" + config.get("MongoProperties", "address") + ":" + config.get("MongoProperties", "port"))
 
     @staticmethod
-    def get_collection(client):
+    def get_collection(client, collection_name):
         db = client.get_database(config.get("MongoProperties", "db_name"))
-        collection = db.get_collection(config.get("MongoProperties", "collection_name"))
+        collection = db.get_collection(collection_name)
         return collection
 
     def load_props(self):
@@ -69,14 +69,30 @@ class FeatureVectorGeneration():
         meta_key_cluster = set()
         key_cluster = set()
 
-        for each in value:
-            for here in each.split("\\"):
-                key_cluster.add(here.lower())
+        if isinstance(value, dict):
+            for each in value:
+                inner_list = value.get(each)
+                for x in inner_list:
+                    if isinstance(x, str):
+                        [key_cluster.add(here.lower()) for here in x.split("\\")]
+                    if isinstance(x, int):
+                        key_cluster.add(x)
+
+        if isinstance(value, list):
+            for each in value:
+                if isinstance(each, int):
+                    key_cluster.add(each)
+                if isinstance(each, str):
+                    for here in each.split("\\"):
+                        key_cluster.add(here.lower())
 
         fingerprint = []
         for each in meta_list:
-            for here in each.split("\\"):
-                meta_key_cluster.add(here.lower())
+            if isinstance(each, int):
+                meta_key_cluster.add(each)
+            if isinstance(each, str):
+                for here in each.split("\\"):
+                    meta_key_cluster.add(here.lower())
 
         for each in meta_key_cluster:
             if each in key_cluster:
@@ -154,20 +170,40 @@ class FeatureVectorGeneration():
                 self.executed_commands_cluster.add(here.lower())
 
         if key == "domains":
-            for here in value:
-                self.domain_cluster.add(here.lower())
+            if isinstance(value, dict):
+                for here in value:
+                    inner_list = value.get(here)
+                    [self.domain_cluster.add(x) for x in inner_list]
+            if isinstance(value, list):
+                for here in value:
+                    self.domain_cluster.add(here.lower())
 
         if key == "udp":
-            for here in value:
-                self.udp_cluster.add(str(here).lower())
+            if isinstance(value, dict):
+                for here in value:
+                    inner_list = value.get(here)
+                    [self.udp_cluster.add(x) for x in inner_list]
+            if isinstance(value, list):
+                for here in value:
+                    self.udp_cluster.add(here.lower())
 
         if key == "hosts":
-            for here in value:
-                self.hosts_cluster.add(here.lower())
+            if isinstance(value, dict):
+                for here in value:
+                    inner_list = value.get(here)
+                    [self.hosts_cluster.add(x) for x in inner_list]
+            if isinstance(value, list):
+                for here in value:
+                    self.hosts_cluster.add(here.lower())
 
         if key == "dns":
-            for here in value:
-                self.dns_cluster.add(here.lower())
+            if isinstance(value, dict):
+                for here in value:
+                    inner_list = value.get(here)
+                    [self.dns_cluster.add(x) for x in inner_list]
+            if isinstance(value, list):
+                for here in value:
+                    self.dns_cluster.add(here.lower())
 
         if key == "dirents":
             for here in value:
@@ -270,7 +306,8 @@ class FeatureVectorGeneration():
         # path = CUCKOO_ROOT + "/cluster/"
         path = _current_dir + "/cluster/"
         log.info("Cluster Path: {0}".format(path))
-        return path, _current_dir
+        # return path, _current_dir
+        return "/Users/satwik/Desktop/VS_ME/cluster/"
 
     def get_path(self):
         path = self.cuckoo_root + "/fingerprint/"
@@ -287,18 +324,59 @@ class FeatureVectorGeneration():
         This is a one time process and once the dumps are created, the next iteration will use these dumps to prepare the next set of dumps.
         :return:
         """
-        pi.dump(self.behavior_dump, open("Meta_Behavior.dump", "w"))
-        pi.dump(self.network_dump, open("Meta_Network.dump", "w"))
-        pi.dump(self.static_dump, open("Meta_Static.dump", "w"))
+        meta_dict = dict()
+
+        files_dict = dict()
+        keys_dict = dict()
+        summary_dict = dict()
+        mutexes_dict = dict()
+        exec_commands_dict = dict()
+
+        files_dict['files'] = self.files_cluster
+        keys_dict['keys'] = self.keys_cluster
+        summary_dict['summary'] = self.summary_cluster
+        mutexes_dict['mutexes'] = self.mutexes_cluster
+        exec_commands_dict['executed_commands'] = self.executed_commands_cluster
+
+        domains_dict = dict()
+        udp_dict = dict()
+        hosts_dict = dict()
+        dns_dict = dict()
+
+        domains_dict['domains'] = self.domain_cluster
+        udp_dict['udp'] = self.udp_cluster
+        hosts_dict['hosts'] = self.hosts_cluster
+        dns_dict['dns'] = self.dns_cluster
+
+        dirents_dict = dict()
+        fn_name_dict = dict()
+        dlls_dict = dict()
+        sections_dict = dict()
+        peid_sign_dict = dict()
+
+        dirents_dict['dirents'] = self.dirents_cluster
+        fn_name_dict['fn_name'] = self.fn_name_cluster
+        dlls_dict['dlls'] = self.dlls_cluster
+        sections_dict['sections'] = self.sections_cluster
+        peid_sign_dict['peid_signatures'] = self.peid_signatures_cluster
+
+        meta_dict['behavior'] = [files_dict, keys_dict, summary_dict, mutexes_dict, exec_commands_dict]
+        meta_dict['network'] = [domains_dict, udp_dict, hosts_dict, dns_dict]
+        meta_dict['static'] = [dirents_dict, fn_name_dict, dlls_dict, sections_dict, peid_sign_dict]
+
+        collection.remove(
+            {'$and': [{'behavior': {'$exists': True}}, {'network': {'$exists': True}}, {'static': {'$exists': True}}]})
+        collection.insert_one(meta_dict)
+        pi.dump(meta_dict, open("Meta_cluster.dump", "w"))
 
     @staticmethod
     def load_dump(cluster_path, dump_name):
         return pi.load(open(cluster_path + dump_name))
 
     def main(self):
-        self.cluster_path, self.cuckoo_root = self.get_cluster_path()
+        self.cluster_path = self.get_cluster_path()
 
-        collection = self.get_collection(self.get_client())
+        collection = self.get_collection(self.get_client(), config.get("MongoProperties", "meta_collection"))
 
         behavior_keys, network_keys, static_keys = self.load_props()
 
@@ -318,12 +396,9 @@ class FeatureVectorGeneration():
 
         self.create_cluster_dumps(collection)
 
-        x = 0
         for each in behavior_keys:
-            x += 1
             self.behavior_dump = self.load_dump(self.cluster_path, each)
             self.behavior_fp()
-            print(x)
 
         for each in network_keys:
             self.network_dump = self.load_dump(self.cluster_path, each)
@@ -333,17 +408,15 @@ class FeatureVectorGeneration():
             self.static_dump = self.load_dump(self.cluster_path, each)
             self.static_fp()
 
-        with open(self.get_path() + 'mycsvfile.csv', 'wb') as csv_file:
-            writer = csv.writer(csv_file)
-            for key, value in self.fingerprint.items():
-                ans = ""
-                for each in value:
-                    ans += ''.join(each.values())
-                writer.writerow([key, ans])
-
-        f1 = open(self.get_path() + "fingerprint.fp", "wa")
-        pi.dump(self.fingerprint, f1)
-        f1.close()
+        collection = self.get_collection(self.get_client(), config.get("MongoProperties", "fingerprint_collection"))
+        for key, value in self.fingerprint.items():
+            try:
+                d = dict()
+                d[key] = value
+                collection.remove({key: {'$exists': True}})
+                collection.insert_one(d)
+            except Exception as e:
+                print("Exception is {} , \n, caused by {}".format(e, key))
 
 
 if __name__ == "__main__":
