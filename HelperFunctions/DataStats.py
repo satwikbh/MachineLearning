ca4058dc36109d192fa87f1d699ebcc8b4be02c9
satwik <satwik_bh@hotmail.com>
2017-07-23 23:13:10 +0530

    Fixed small bug.

--- HelperFunctions/DataStats.py
+++ HelperFunctions/DataStats.py
@@ -4,6 +4,7 @@ from HelperFunctions.HelperFunction import HelperFunction
 from time import time
 
 import hickle as hkl
+import numpy as np
 
 
 class DataStats:
@@ -17,19 +18,22 @@ class DataStats:
         fv_csc = fv.tocsc()
         index_pointer = fv_csc.indptr
         new_index_pointer = list()
-        count = 0
-        for each_index in xrange(1, index_pointer.shape[0]):
-            count = each_index - count
-            new_index_pointer.append(each_index)
-        new_index_pointer.append(fv_csc[:, -1].nnz)
+
+        for x in xrange(1, index_pointer.shape[0]):
+            value = index_pointer[x] - index_pointer[x - 1]
+            new_index_pointer.append(value)
         return new_index_pointer
 
-    @staticmethod
-    def sum_up(partial_index_pointer, col_wise_dist):
-        assert len(partial_index_pointer) == len(col_wise_dist)
-        for x in xrange(len(partial_index_pointer)):
-            col_wise_dist[x] = col_wise_dist[x] + partial_index_pointer[x]
-        return col_wise_dist
+    def sum_up(self, partial_index_pointer, col_wise_dist):
+        self.log.info(
+            "partial_index_pointer : {}\tcol_wise_dist : {}".format(len(partial_index_pointer), len(col_wise_dist)))
+        if len(col_wise_dist) == 0:
+            return partial_index_pointer
+        else:
+            assert len(partial_index_pointer) == len(col_wise_dist)
+            for x in xrange(len(partial_index_pointer)):
+                col_wise_dist[x] = col_wise_dist[x] + partial_index_pointer[x]
+            return col_wise_dist
 
     def get_stats(self, list_of_files):
         col_wise_dist = list()
@@ -38,6 +42,7 @@ class DataStats:
             fv = hkl.load(each_file)
             partial_index_pointer = self.stats(fv)
             col_wise_dist = self.sum_up(partial_index_pointer, col_wise_dist)
+            del fv
         return col_wise_dist
 
     def main(self):
@@ -45,8 +50,9 @@ class DataStats:
         self.log.info("Generating column wise count of non zero elements")
         feature_vector_path = self.config['dimensionality_reduction']['feature_vector_path']
         feature_vector = self.helper.get_files_starts_with_extension("feature_vector_part-", feature_vector_path)
+        self.log.info("Total number of files : {}".format(len(feature_vector)))
         col_wise_dist = self.get_stats(feature_vector)
-        hkl.dump(col_wise_dist, open("col_wise_dist.dump", "w"))
+        hkl.dump(np.asarray(col_wise_dist), open("col_wise_dist.dump", "w"))
         self.log.info("Total time for execution : {}".format(time() - start_time))
 
 
