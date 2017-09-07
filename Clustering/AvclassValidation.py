439663822aae13f4fefe2d59fccdd4c3156d4dbe
satwik <satwik_bh@hotmail.com>
2017-09-07 16:03:17 +0530

    Handled division by zero error.

--- Clustering/AvclassValidation.py
+++ Clustering/AvclassValidation.py
@@ -72,7 +72,10 @@ class AvclassValidation:
             try:
                 new_family_names = [x for x in family_names if "SINGLETON" not in x]
                 unique = len(set(new_family_names))
-                cluster_dist[cluster_label] = 1.0 - (unique * 1.0 / len(new_family_names))
+                if len(new_family_names) > 0:
+                    cluster_dist[cluster_label] = 1.0 - (unique * 1.0 / len(new_family_names))
+                else:
+                    cluster_dist[cluster_label] = 1.0 - (unique * 1.0 / 10 ** 9)
             except Exception as e:
                 self.log.error("Error : {}".format(e))
         return cluster_dist
