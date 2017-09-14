30326ff9aff4796c57725aa18ec393cc426beaf2
satwik <satwik_bh@hotmail.com>
2017-09-14 05:51:18 +0530

    Modified code for PCA and added code for Kernel PCA.

--- LinearDimensionalityReduction/PrincipalComponentAnalysis.py
+++ LinearDimensionalityReduction/PrincipalComponentAnalysis.py
@@ -4,7 +4,6 @@ import numpy as np
 from time import time
 
 from sklearn.decomposition.pca import PCA
-from sklearn.decomposition.kernel_pca import KernelPCA
 
 from Utils.LoggerUtil import LoggerUtil
 from Utils.ConfigUtil import ConfigUtil
@@ -12,9 +11,10 @@ from HelperFunctions.HelperFunction import HelperFunction
 from PrepareData.LoadData import LoadData
 from Clustering.DBScanClustering import DBScanClustering
 from Clustering.HDBScanClustering import HDBScanClustering
+from Clustering.EstimateClusterParams import EstimateClusterParams
 
 
-class PrincipalComponentAnanlysis:
+class PrincipalComponentAnalysis:
     def __init__(self):
         self.log = LoggerUtil(self.__class__.__name__).get()
         self.config = ConfigUtil.get_config_instance()
@@ -22,56 +22,61 @@ class PrincipalComponentAnanlysis:
         self.dbscan = DBScanClustering()
         self.hdbscan = HDBScanClustering()
         self.helper = HelperFunction()
+        self.estimate_params = EstimateClusterParams()
 
-    def pca(self, input_matrix, k_value, randomized=False):
+    def pca(self, input_matrix, num_rows, randomized=False):
         """
         Performs PCA. Here SVD will be solved using randomized algorithms.
         Function is not necessary explicitly as pca has svd_solver set to 'auto' by default which for higher dimension data performs randomized SVD.
         :param input_matrix: The input matrix ndarray form.
-        :param k_value: The number of principal components to keep.
+        :param num_rows: The number of rows.
         :param randomized: Perform randomized svd under the hood if true.
         Disabled by default.
         :return:
         """
         self.log.info("Entering the {} class".format(self.pca.__name__))
-        if randomized:
-            pca = PCA(n_components=k_value, svd_solver='randomized')
-        else:
-            pca = PCA(n_components=k_value)
-        reduced_matrix = pca.fit_transform(input_matrix)
+        results_path = self.config['results']['params']['pca']
+        reconstruction_error = self.config['models']['pca']['reconstruction_error']
+        n_components_list = [1000, 5100, 100]
+        best_params = dict()
+        error_prev = 0.0
+        for n_components in n_components_list:
+            try:
+                if randomized:
+                    pca = PCA(n_components=n_components, svd_solver='randomized')
+                else:
+                    pca = PCA(n_components=n_components)
+                reduced_matrix = pca.fit_transform(input_matrix)
+                reconstructed_matrix = pca.inverse_transform(reduced_matrix)
+                error_curr = self.helper.mean_square_error(reconstructed_matrix, input_matrix)
+                best_params['n_components_' + str(n_components)] = error_curr
+                if (error_curr - error_prev) * 100 < reconstruction_error:
+                    json.dump(best_params, open(results_path + "/" + "best_params_pca_" + str(num_rows) + ".json", "w"))
+                    break
+            except Exception as e:
+                self.log.error("Error : {}".format(e))
         self.log.info("Exiting the {} class".format(self.pca.__name__))
-        return reduced_matrix
+        return reduced_matrix, n_components
 
-    def kernel_pca(self, input_matrix, k_value):
-        """
-        Performs the Kernel Principal Component Reduction.
-        :param input_matrix:
-        :param k_value:
-        :return:
-        """
-        self.log.info("Inside the {} class".format(self.pca.__name__))
-        kpca = KernelPCA(kernel="rbf", fit_inverse_transform=True, gamma=10, n_components=k_value)
-        X_kpca = kpca.fit_transform(input_matrix)
-        X_back = kpca.inverse_transform(X_kpca)
-        self.log.info("Exiting the {} class".format(self.pca.__name__))
-        return X_back
-
-    def main(self):
+    def main(self, cluster_estimation=True):
         """
         The main method.
         :return:
         """
-        model_path = self.config["models"]["pca"]
-        results_path = self.config["results"]["iterations"]["pca"]
+        pca_model_path = self.config["models"]["pca"]["model_path"]
+        pca_results_path = self.config["results"]["iterations"]["pca"]
+
         start_time = time()
         num_rows = 25000
         input_matrix, input_matrix_indices = self.load_data.main(num_rows=num_rows)
-        # 1000 because we analyzed for various num_rows ranging from 100 to 25000.
-        # We found 1st 1000 principal components are more than enough.
-        reduced_matrix = self.pca(input_matrix.toarray(), 1000, randomized=True)
-        self.log.info("Saving the model at : {}".format(model_path))
-        fname = model_path + "/" + "pca_reduced_matrix_" + str(num_rows)
+        reduced_matrix, n_components = self.pca(input_matrix=input_matrix.toarray(), num_rows=num_rows, randomized=True)
+        self.log.info("Saving the PCA model at : {}".format(pca_model_path))
+        fname = pca_model_path + "/" + "pca_reduced_matrix_" + str(num_rows)
         np.save(file=fname, arr=reduced_matrix)
+
+        if cluster_estimation is False:
+            return n_components
+
         eps_list = self.helper.frange(0.1, 1.1, 0.1)
         min_samples_list = range(2, 22, 2)
         min_cluster_size_list = range(2, 22, 2)
@@ -93,12 +98,12 @@ class PrincipalComponentAnanlysis:
         final_accuracies["dbscan"] = dbscan_accuracy_params
         final_accuracies["hdbscan"] = hdbscan_accuracy_params
 
-        pi.dump(final_accuracies, open(results_path + "/" + "pca_results_" + str(num_rows) + ".pickle", "w"))
-        json.dump(final_accuracies, open(results_path + "/" + "pca_results_" + str(num_rows) + ".json", "w"))
+        pi.dump(final_accuracies, open(pca_results_path + "/" + "pca_results_" + str(num_rows) + ".pickle", "w"))
+        json.dump(final_accuracies, open(pca_results_path + "/" + "pca_results_" + str(num_rows) + ".json", "w"))
 
         self.log.info("Total time taken : {}".format(time() - start_time))
 
 
 if __name__ == '__main__':
-    pca = PrincipalComponentAnanlysis()
-    pca.main()
+    pca = PrincipalComponentAnalysis()
+    pca.main(cluster_estimation=True)
---
+++ NonLinearDimensionalityReduction/KPCA.py
@@ -0,0 +1,134 @@
+import json
+import pickle as pi
+import numpy as np
+from time import time
+
+from sklearn.decomposition.kernel_pca import KernelPCA
+
+from Utils.LoggerUtil import LoggerUtil
+from Utils.ConfigUtil import ConfigUtil
+from HelperFunctions.HelperFunction import HelperFunction
+from PrepareData.LoadData import LoadData
+from Clustering.DBScanClustering import DBScanClustering
+from Clustering.HDBScanClustering import HDBScanClustering
+from Clustering.EstimateClusterParams import EstimateClusterParams
+from LinearDimensionalityReduction.PrincipalComponentAnalysis import PrincipalComponentAnalysis
+
+
+class KPCA:
+    def __init__(self):
+        self.log = LoggerUtil(self.__class__.__name__).get()
+        self.config = ConfigUtil.get_config_instance()
+        self.load_data = LoadData()
+        self.dbscan = DBScanClustering()
+        self.hdbscan = HDBScanClustering()
+        self.helper = HelperFunction()
+        self.estimate_params = EstimateClusterParams()
+        self.pca = PrincipalComponentAnalysis()
+
+    def kernel_pca(self, input_matrix, n_components):
+        """
+        Performs the Kernel Principal Component Reduction.
+        :param input_matrix:
+        :param n_components:
+        :return:
+        """
+        global reduced_matrix
+        self.log.info("Inside the {} class".format(self.kernel_pca.__name__))
+        reconstruction_error = self.config['models']['kpca']['reconstruction_error']
+        kernels = ['poly', 'rbf', 'sigmoid']
+        gamma_list = self.helper.frange(0.0001, 0.0051, 0.0001)
+        degree_list = self.helper.frange(1, 51, 1)
+        results_path = self.config['results']['params']['kpca']
+        best_params = dict()
+        error_prev = 0.0
+        for kernel in kernels:
+            try:
+                best_params[kernel] = {}
+                if kernel is 'poly':
+                    for degree in degree_list:
+                        self.log.info("Params are \nKernel : {} \t Degree : {}".format(kernel, degree))
+                        kpca = KernelPCA(degree=degree, kernel=kernel, fit_inverse_transform=True,
+                                         n_components=n_components, n_jobs=30)
+                        reduced_matrix = kpca.fit_transform(input_matrix)
+                        projected_matrix = kpca.inverse_transform(reduced_matrix)
+                        error_curr = self.helper.mean_square_error(input_matrix, projected_matrix)
+                        best_params[kernel]['degree_' + str(degree)] = error_curr
+                        if (error_curr - error_prev) * 100 < reconstruction_error:
+                            break
+                if kernel in ['rbf', 'sigmoid']:
+                    for gamma in gamma_list:
+                        self.log.info("Params are \nKernel : {} \t Gamma : {}".format(kernel, gamma))
+                        kpca = KernelPCA(gamma=gamma, kernel=kernel, fit_inverse_transform=True,
+                                         n_components=n_components,
+                                         n_jobs=30)
+                        reduced_matrix = kpca.fit_transform(input_matrix)
+                        projected_matrix = kpca.inverse_transform(reduced_matrix)
+                        error_curr = self.helper.mean_square_error(input_matrix, projected_matrix)
+                        best_params[kernel]['gamma_' + str(gamma)] = error_curr
+                        if (error_curr - error_prev) * 100 < reconstruction_error:
+                            break
+            except Exception as e:
+                self.log.error("Error : {}".format(e))
+        json.dump(best_params, open(results_path + "/" + "best_params_kpca.json", "w"))
+        self.log.info("Exiting the {} class".format(self.kernel_pca.__name__))
+        return reduced_matrix
+
+    def get_n_components(self, num_rows, pca_results_path):
+        fname = pca_results_path + "/" + "best_params_pca_" + str(num_rows) + ".json"
+        if self.helper.is_file_present(fname):
+            best_params = json.load(open(fname))
+            best_param_value = min(best_params, key=best_params.get)
+            n_components = best_param_value.split('n_components_')[1]
+        else:
+            n_components = self.pca.main(cluster_estimation=False)
+        return n_components
+
+    def main(self):
+        """
+        The main method.
+        :return:
+        """
+        start_time = time()
+        kpca_model_path = self.config["models"]["kpca"]["model_path"]
+        kpca_results_path = self.config["results"]["iterations"]["kpca"]
+        pca_results_path = self.config['results']['params']['kpca']
+
+        num_rows = 25000
+        input_matrix, input_matrix_indices = self.load_data.main(num_rows=num_rows)
+        n_components = self.get_n_components(num_rows, pca_results_path)
+        reduced_matrix = self.kernel_pca(input_matrix=input_matrix.toarray(), n_components=n_components)
+        self.log.info("Saving the K-PCA model at : {}".format(kpca_model_path))
+        fname = kpca_model_path + "/" + "kpca_reduced_matrix_" + str(num_rows)
+        np.save(file=fname, arr=reduced_matrix)
+
+        eps_list = self.helper.frange(0.1, 1.1, 0.1)
+        min_samples_list = range(2, 22, 2)
+        min_cluster_size_list = range(2, 22, 2)
+
+        final_accuracies = dict()
+
+        dbscan_accuracy_params = self.dbscan.dbscan_cluster(input_matrix=reduced_matrix,
+                                                            input_matrix_indices=input_matrix_indices,
+                                                            eps_list=eps_list,
+                                                            min_samples_list=min_samples_list)
+
+        hdbscan_accuracy_params = self.hdbscan.hdbscan_cluster(input_matrix=reduced_matrix,
+                                                               input_matrix_indices=input_matrix_indices,
+                                                               min_cluster_size_list=min_cluster_size_list)
+
+        self.log.info("DBScan Accuracy : {}".format(dbscan_accuracy_params))
+        self.log.info("HDBScan Accuracy : {}".format(hdbscan_accuracy_params))
+
+        final_accuracies["dbscan"] = dbscan_accuracy_params
+        final_accuracies["hdbscan"] = hdbscan_accuracy_params
+
+        pi.dump(final_accuracies, open(kpca_results_path + "/" + "kpca_results_" + str(num_rows) + ".pickle", "w"))
+        json.dump(final_accuracies, open(kpca_results_path + "/" + "kpca_results_" + str(num_rows) + ".json", "w"))
+
+        self.log.info("Total time taken : {}".format(time() - start_time))
+
+
+if __name__ == '__main__':
+    kpca = KPCA()
+    kpca.main()
