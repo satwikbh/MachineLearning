89cdcf87c1f0f6b99032c88a1870a3c6fd6861e3
satwik <satwik_bh@hotmail.com>
2017-09-22 13:07:21 +0530

    Added MDS and KPCA code.

--- LinearDimensionalityReduction/MultiDimensionalScaling.py
+++ LinearDimensionalityReduction/MultiDimensionalScaling.py
@@ -1,56 +1,95 @@
 import json
+import numpy as np
 
 from time import time
+from sklearn.metrics.pairwise import euclidean_distances, cosine_distances
+from sklearn.manifold import MDS
 
 from Utils.LoggerUtil import LoggerUtil
 from Utils.ConfigUtil import ConfigUtil
+from HelperFunctions.HelperFunction import HelperFunction
 from PrepareData.LoadData import LoadData
+from LinearDimensionalityReduction.PrincipalComponentAnalysis import PrincipalComponentAnalysis
 
 
-class DiscriminantAnalysis:
+class MultiDimensionalScaling:
     def __init__(self):
         self.log = LoggerUtil(self.__class__.__name__).get()
         self.config = ConfigUtil.get_config_instance()
         self.load_data = LoadData()
+        self.helper = HelperFunction()
+        self.pca = PrincipalComponentAnalysis()
+
+    def perform_mds(self, n_components, input_matrix, seed):
+        start_time = time()
+        metrics = ['euclidean', 'cosine']
+        self.log.info("The metrics enabled for MDS are : {}".format(metrics))
+        mds = MDS(n_components=n_components, metric=True,
+                  max_iter=3000, eps=1e-12,
+                  dissimilarity="precomputed",
+                  random_state=seed, n_jobs=-1,
+                  n_init=1)
+
+        nmds = MDS(n_components=n_components, metric=False,
+                   max_iter=3000, eps=1e-12,
+                   dissimilarity="precomputed",
+                   random_state=seed, n_jobs=-1,
+                   n_init=1)
+
+        mds_reduced_matrix_list = list()
+        nmds_reduced_matrix_list = list()
+
+        for distance_metric in metrics:
+            if 'euclidean' is distance_metric:
+                similarities = euclidean_distances(input_matrix, input_matrix)
+            if 'cosine' is distance_metric:
+                similarities = cosine_distances(input_matrix, input_matrix)
+            self.log.info("Fitting MDS with {} distance metric".format(distance_metric))
+            mds_reduced_matrix = mds.fit_transform(similarities)
+            mds_reduced_matrix_list.append(mds_reduced_matrix)
+
+            self.log.info("Fitting N-MDS with {} distance metric".format(distance_metric))
+            nmds_reduced_matrix = nmds.fit_transform(similarities)
+            nmds_reduced_matrix_list.append(nmds_reduced_matrix)
+
+        mds_reduced_matrix_list = np.asarray(mds_reduced_matrix_list)
+        nmds_reduced_matrix_list = np.asarray(nmds_reduced_matrix_list)
+
+        self.log.info("Time taken to perform_mds is {}".format(time() - start_time))
+        return mds_reduced_matrix_list, nmds_reduced_matrix_list
+
+    def get_n_components(self, num_rows, pca_results_path):
+        fname = pca_results_path + "/" + "best_params_pca_" + str(num_rows) + ".json"
+        if self.helper.is_file_present(fname):
+            best_params = json.load(open(fname))
+            best_param_value = min(best_params, key=best_params.get)
+            n_components = best_param_value.split('n_components_')[1]
+        else:
+            n_components = self.pca.main(num_rows=num_rows, cluster_estimation=False)
+        self.log.info("Number of components : {}".format(n_components))
+        return n_components
 
     def main(self):
         start_time = time()
-        plot_path = self.config['plots']['lle']
-        model_path = self.config['models']['lle']
-        results_path = self.config['results']['iterations']['lle']
 
         num_rows = 25000
-        batch_size = 1000
-        counter = 0
-        index = 0
-        final_accuracies = dict()
+        seed = np.random.RandomState(seed=3)
+
+        mds_model_path = self.config["models"]["mds"]["model_path"]
+        mds_results_path = self.config["results"]["iterations"]["mds"]
+        pca_results_path = self.config["results"]["params"]["pca"]
 
         input_matrix, input_matrix_indices = self.load_data.main(num_rows=num_rows)
-        while counter < num_rows:
-            self.log.info("Discriminant Analysis on iteration #{}".format(index))
-            partial_matrix = input_matrix[counter: counter + batch_size]
-            partial_matrix_indices = input_matrix_indices[counter: counter + batch_size]
-            counter += batch_size
-            da_accuracies = dict()
+        n_components = self.get_n_components(num_rows, pca_results_path)
+        mds_reduced_matrix_list, nmds_reduced_matrix_list = self.perform_mds(n_components=n_components,
+                                                                             input_matrix=input_matrix, seed=seed)
+        self.log.info("Saving the MDS model at : {}".format(mds_model_path))
+        mds_fname = mds_model_path + "/" + "mds_reduced_matrix_" + str(num_rows)
+        nmds_fname = mds_model_path + "/" + "nmds_reduced_matrix_" + str(num_rows)
+        np.savez_compressed(file=mds_fname, arr=mds_reduced_matrix_list)
+        np.savez_compressed(file=nmds_fname, arr=nmds_reduced_matrix_list)
 
-            dbscan_accuracies, hdbscan_accuracies = self.lda.perform__lda(n_components=3, iteration=index,
-                                                                          partial_matrix=partial_matrix,
-                                                                          partial_matrix_indices=partial_matrix_indices,
-                                                                          plot_path=plot_path, model_path=model_path)
+        # TODO
+        # Add clustering code.
 
-            da_accuracies["lda"] = [dbscan_accuracies, hdbscan_accuracies]
-
-            dbscan_accuracies, hdbscan_accuracies = self.qda.perform_qda(n_components=3, iteration=index,
-                                                                         partial_matrix=partial_matrix,
-                                                                         partial_matrix_indices=partial_matrix_indices,
-                                                                         plot_path=plot_path, model_path=model_path)
-
-            dbscan_accuracies['qda'] = [dbscan_accuracies, hdbscan_accuracies]
-
-            self.log.info("DBScan Accuracy for Iteration #{} : {}".format(index, dbscan_accuracies))
-            self.log.info("HDBScan Accuracy for Iteration #{} : {}".format(index, hdbscan_accuracies))
-            final_accuracies[index] = dbscan_accuracies
-            index += 1
-
-        json.dump(final_accuracies, open(results_path + "/" + "results_" + str(num_rows) + ".json", "w"))
         self.log.info("Total time taken : {}".format(time() - start_time))
