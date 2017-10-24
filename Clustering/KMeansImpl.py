ef8fa1749c5556143519310246fe67e8d04db3be
satwik <satwik_bh@hotmail.com>
2017-10-24 12:49:19 +0530

    Structure changes for KMeans.

--- Clustering/KMeansImpl.py
+++ Clustering/KMeansImpl.py
@@ -43,7 +43,7 @@ class KMeansImpl:
 
     def perform_kmeans(self, input_matrix, input_matrix_indices):
         results_list = list()
-        for k_value in range(2, 500):
+        for k_value in range(2, 400):
             cluster_labels = self.core_model(input_matrix, k_value)
             cluster_accuracy, input_labels = self.validation.main(cluster_labels, input_matrix_indices)
             s_score = self.metric.silhouette_score(input_matrix, cluster_labels)
@@ -55,37 +55,59 @@ class KMeansImpl:
         results_array = np.asarray(results_list)
         return results_array
 
+    def prepare_kmeans(self, dr_matrices, input_matrix_indices):
+        dr_results_array = dict()
+        for dr_name, dr_matrix in dr_matrices.items():
+            if dr_name is "pca":
+                self.log.info("Performing K-Means on PCA")
+            elif dr_name is "mds":
+                self.log.info("Performing K-Means on Metric MDS")
+            elif dr_name is "nmds":
+                self.log.info("Performing K-Means on Non-Metric MDS")
+            elif dr_name is "tsne":
+                self.log.info("Performing K-Means on TSNE")
+            else:
+                self.log.error("Dimensionality Reduction technique employed is not supported!!!")
+            dr_results_array[dr_name] = self.perform_kmeans(dr_matrix, input_matrix_indices)
+        return dr_results_array
+
     @staticmethod
     def get_dr_matrices(pca_model_path, mds_model_path, tsne_model_path, num_rows):
+        dr_matrices = dict()
         pca_file_name = pca_model_path + "/" + "pca_reduced_matrix_" + str(num_rows) + ".npy"
         pca_reduced_matrix = np.load(pca_file_name)
+        dr_matrices["pca"] = pca_reduced_matrix
 
         mds_file_name = mds_model_path + "/" + "mds_reduced_matrix_" + str(num_rows) + ".npz"
-        nmds_file_name = mds_model_path + "/" + "nmds_reduced_matrix_" + str(num_rows) + ".npz"
+        mds_reduced_matrix = np.load(mds_file_name)['arr'][0]
+        dr_matrices["mds"] = mds_reduced_matrix
 
-        mds_reduced_matrix = np.load(mds_file_name)
-        nmds_reduced_matrix = np.load(nmds_file_name)
+        nmds_file_name = mds_model_path + "/" + "nmds_reduced_matrix_" + str(num_rows) + ".npz"
+        nmds_reduced_matrix = np.load(nmds_file_name)['arr'][0]
+        dr_matrices["nmds"] = nmds_reduced_matrix
 
         tsne_file_name = tsne_model_path + "/" + "tsne_reduced_matrix_" + str(num_rows) + ".npz"
         tsne_reduced_matrix = np.load(tsne_file_name)
+        dr_matrices["tsne"] = tsne_reduced_matrix
 
-        return pca_reduced_matrix, mds_reduced_matrix, nmds_reduced_matrix, tsne_reduced_matrix
+        return dr_matrices
 
-    @staticmethod
-    def save_results(num_rows, pca_results_path, pca_results_array,
-                     mds_results_path, mds_results_array, nmds_results_array,
-                     tsne_results_path, tsne_results_array):
-        pca_fname = pca_results_path + "kmeans_pca_" + str(num_rows)
-        np.savez_compressed(pca_fname, pca_results_array)
-
-        mds_fname = mds_results_path + "kmeans_mds_" + str(num_rows)
-        np.savez_compressed(mds_fname, mds_results_array)
-
-        nmds_fname = mds_results_path + "kmeans_nmds_" + str(num_rows)
-        np.savez_compressed(nmds_fname, nmds_results_array)
-
-        tsne_fname = tsne_results_path + "kmeans_tsne_" + str(num_rows)
-        np.savez_compressed(tsne_fname, tsne_results_array)
+    def save_results(self, num_rows, dr_results_array, pca_results_path, mds_results_path, tsne_results_path):
+        for dr_name, dr_results in dr_results_array.items():
+            if dr_name is "pca":
+                pca_fname = pca_results_path + "kmeans_pca_" + str(num_rows)
+                np.savez_compressed(pca_fname, dr_results)
+            elif dr_name is "mds":
+                mds_fname = mds_results_path + "kmeans_mds_" + str(num_rows)
+                np.savez_compressed(mds_fname, dr_results)
+            elif dr_name is "nmds":
+                nmds_fname = mds_results_path + "kmeans_nmds_" + str(num_rows)
+                np.savez_compressed(nmds_fname, dr_results)
+            elif dr_name is "tsne":
+                tsne_fname = tsne_results_path + "kmeans_tsne_" + str(num_rows)
+                np.savez_compressed(tsne_fname, dr_results)
+            else:
+                self.log.error("Dimensionality Reduction technique employed is not supported!!!")
 
     def main(self, num_rows):
         start_time = time()
@@ -98,24 +120,10 @@ class KMeansImpl:
         tsne_results_path = self.config["results"]["iterations"]["tsne"]
         input_matrix, input_matrix_indices = self.load_data.main(num_rows=num_rows)
 
-        pca_reduced_matrix, mds_reduced_matrix, nmds_reduced_matrix, tsne_reduced_matrix = self.get_dr_matrices(
-            pca_model_path, mds_model_path, tsne_model_path, num_rows)
+        dr_matrices = self.get_dr_matrices(pca_model_path, mds_model_path, tsne_model_path, num_rows)
+        dr_results_array = self.prepare_kmeans(dr_matrices, input_matrix_indices)
 
-        self.log.info("Performing K-Means on PCA")
-        pca_results_array = self.perform_kmeans(pca_reduced_matrix, input_matrix_indices)
-
-        self.log.info("Performing K-Means on Metric MDS")
-        mds_results_array = self.perform_kmeans(mds_reduced_matrix, input_matrix_indices)
-
-        self.log.info("Performing K-Means on Non-Metric MDS")
-        nmds_results_array = self.perform_kmeans(nmds_reduced_matrix, input_matrix_indices)
-
-        self.log.info("Performing K-Means on TSNE")
-        tsne_results_array = self.perform_kmeans(tsne_reduced_matrix, input_matrix_indices)
-
-        self.save_results(num_rows, pca_results_path, pca_results_array,
-                          mds_results_path, mds_results_array, nmds_results_array,
-                          tsne_results_path, tsne_results_array)
+        self.save_results(num_rows, dr_results_array, pca_results_path, mds_results_path, tsne_results_path)
 
         self.log.info("Time taken : {}".format(time() - start_time))
 
