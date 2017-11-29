b3146a292845e0f981ec2cb010b94fd321c98e2e
unknown <satwik_bh@hotmail.com>
2017-11-30 01:48:31 +0530

    Added templates for Scanning using Signature generated

---
+++ Cluster2DB/CuckooQueue.py
@@ -0,0 +1,60 @@
+from Utils.LoggerUtil import LoggerUtil
+from PerformAnalysis import PerformAnalysis
+
+
+class CuckooQueue:
+    """
+    This class will queue the executable for behavioral analysis. This is done using Kafka.
+    The producer produces the message which contains Queue class object and puts it in the channel.
+    Once the executable is analyzed, the result success or failed will be appended in the respective collection.
+    """
+
+    def __init__(self):
+        self.log = LoggerUtil.get(self.__class__.__name__)
+        self.queue = Queue()
+        self.cuckoo_object = PerformAnalysis()
+
+    def producer(self, md5_value, md5_path):
+        """
+        This method adds the executable and its path to the queue collection.
+        Behavioral analysis is performed for elements in this collection.
+        :param md5_value:
+        :param md5_path:
+        :return:
+        """
+        meta_list = self.queue.insert_in_queue(md5_value, md5_path)
+        # TODO : Insert this meta_list item in the channel so that PerformAnalysis class consumer can consume it.
+
+    def main(self, md5_value, md5_path):
+        self.producer(md5_value, md5_path)
+
+
+class Queue:
+    def __init__(self):
+        pass
+
+    def insert_in_queue(self, md5_value, md5_path):
+        """
+        Given a md5 value, this method creates a custom object, inserts the value and returns it.
+        :param md5_path:
+        :param md5_value:
+        :return:
+        """
+        meta_list = list()
+        element = self.create_object(md5_value, md5_path)
+        meta_list.append(element)
+        return meta_list
+
+    @staticmethod
+    def create_object(md5_value, md5_path):
+        element = dict()
+        element["md5"] = md5_value
+        element["fil_path"] = md5_path
+        element["success"] = 0
+        element["failure"] = 0
+        return element
+
+
+if __name__ == '__main__':
+    cuckoo_queue = CuckooQueue()
+    cuckoo_queue.main(md5_path=None, md5_value=None)
---
+++ Cluster2DB/PerformAnalysis.py
@@ -0,0 +1,63 @@
+from Utils.LoggerUtil import LoggerUtil
+
+
+class PerformAnalysis:
+    """
+    This class invokes the scripts neccessary for the behavioral analysis of the malware.
+    The malware executable's path is picked from the collection analysis is done.
+    It appends to the success or failure collections based on the execution status.
+    This class is Asynchronous and doesn't respond to any class.
+    """
+
+    def __init__(self):
+        self.log = LoggerUtil.get(self.__class__.__name__)
+
+    def submit_2_cuckoo(self, file_path):
+        """
+        The executable is submitted to cuckoo sandbox for dynamic analysis using the file_path.
+        It returns success or failure based on cuckoo's response.
+        :param file_path:
+        :return:
+        """
+        if True:
+            return "success"
+        else:
+            return "failure"
+
+    def consume(self, channel_name):
+        # TODO : Insert code to read from channel and get the contents of message. Replace none with that method call.
+        message = dict()
+        self.log.info("Message from Producer : {}".format(message))
+        return message
+
+    def to_book_keeping_collection(self, message, status):
+        """
+        Inserts into success or failure collection based on the status.
+        :param message:
+        :param status:
+        :return:
+        """
+        if status == "success":
+            message["success_count"] += 1
+        else:
+            message["failure_count"] += 1
+        self.log.info("")
+
+    def consumer(self, channel_name):
+        """
+        This consumer consumes from the channel and gets the executable's md5 and its file path.
+        Once the analysis is done,
+        :param channel_name:
+        :return:
+        """
+        message = self.consume(channel_name)
+        status = self.submit_2_cuckoo(file_path=message["md5_path"])
+        self.to_book_keeping_collection(message, status)
+
+    def main(self):
+        pass
+
+
+if __name__ == '__main__':
+    perform_analysis = PerformAnalysis()
+    perform_analysis.main()
